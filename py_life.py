b25f41f2a86a2dd47673613e697c0b5f4edacc88
Gary Mullen <garyfmullen@gmail.com>
2020-04-13 07:16:03 -0700

    Creating Cell Object bases on Size

--- py_life.py
+++ py_life.py
@@ -14,25 +14,41 @@ class Life:
     size = [0, 0]
     end_tick = 0
     current_tick = 0
-
+    cells = []
+    num_cells = 0
+    
     def __init__(self, size, end_tick, seed):
         print(GREEN + "Created Life Universe" + ENDC)
         self.size = size
         self.end_tick = end_tick
         self.seed = seed
         self.current_tick = 0
+        self.create_cells()    
+
+    def create_cells(self):
+        for row in range(self.size[0]):
+            self.cells.append([Cell()])
+            for col in range((self.size[1] - 1)):
+                self.cells[row].append(Cell())
+        self.update_num_cells()
+
+    def get_num_cells(self):
+        return self.num_cells 
+    def update_num_cells(self):
+        nrow = len(self.cells)
+        ncols = len(self.cells[0])
+        self.num_cells = nrow * ncols
 
     def get_size(self):
         return self.size
-        
     def get_end_tick(self):
         return self.ticks
-
+    def get_tick(self):
+        return self.current_tick        
     def inc_ticks(self):
         self.current_tick += 1
 
-    def get_tick(self):
-        return self.current_tick        
+
 
 class Cell:
     'Cell Class'
@@ -40,11 +56,10 @@ class Cell:
     next_state = None 
     neighbours_states = []
 
-    def __init__(self, current_state, next_state, neighbours_states):
-       print(RED + "Created Cell" + ENDC)
-       self.current_state = current_state 
-       self.next_state = next_state
-       self.neighbours_states = neighbours_states  
+    def __init__(self):
+       self.current_state = None 
+       self.next_state = None
+       self.neighbours_states = None  
 
     def get_current_state(self):
         return self.current_state
@@ -62,8 +77,8 @@ class Cell:
 
 
 def main():
-    life = Life([5,5], 100, 0)
-    
+    life = Life([10,10], 100, 0)
+    print(life.get_num_cells())
 
 if __name__ == "__main__":
     main()
